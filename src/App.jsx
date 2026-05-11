31442c2ebb0e587de770be764d51db6a4b44d1d5
Adi Hodzic <adisuo@outlook.dk>
2026-05-11 22:39:30 +0200

    my first app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", fontSize: "40px" }}>
+      Hello Iggy
+    </div>
+  );
+}
+
+export default App;
